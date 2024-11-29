c6985b815c1fe7219ecdb5cf1b5bd5a13a68a7d4
Halle Black <hcb0046@auburn.edu>
2024-11-29 11:29:23 -0600

    Initial Commit

---
+++ ShelterApp/ShelterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShelterAppApp.swift
+//  ShelterApp
+//
+//  Created by Halle Winters on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ShelterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
